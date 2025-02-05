5f393cfeec3355326702d3173f38a594564f17a0
vvduth <ducthai060501@gmail.com>
2025-02-05 16:04:31 +0200

    initialize React app with essential configuration and files

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client";
+import { useState } from "react";
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  }
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button
+          onClick={onClick}
+        >Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+root.render(<App />);
